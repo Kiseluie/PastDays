b046eaf7bc9dbba520fb09ce5917ab0033043425
ShadowFox131105 <dima.sv.1311200593@gmail.com>
2024-05-08 19:31:00 +0300

    Сделал прототип скримера
    персонаж останавливается и и смотрит на лицо тени при поимке
    Не хватает тряски экрана

--- Assets/_MainProject/Scripts/ShadowAI.cs
+++ Assets/_MainProject/Scripts/ShadowAI.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
@@ -10,31 +9,45 @@ public class ShadowAI : MonoBehaviour
 
     public NavMeshAgent Entity;
     public Transform player;
-    public float catсhingRange = 2f;
+    public float catchingRange = 2f;
     public float chaseRange = 10;
+    public float heightOffset = 1.0f;
+    public Transform mainCamera; 
+    public Transform ShadowMonster;
 
-    private bool _jumpScarePlayed = false; // Флаг для отслеживания проигрывания звука
+    private FirstPersonController firstPersonController;
+    private Rigidbody rb;
+    public GameObject playerObject;
+
+    private bool _jumpScarePlayed = false;
 
     void Start()
     {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        firstPersonController = playerObject.GetComponent<FirstPersonController>();
+        rb = playerObject.GetComponent<Rigidbody>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        mainCamera = Camera.main.transform;
     }
 
     void Update()
     {
-        float destination = Vector3.Distance(Entity.transform.position, player.position);
+        float distanceToPlayer = Vector3.Distance(Entity.transform.position, player.position);
 
-        if (!_jumpScarePlayed && destination <= catсhingRange)
+        if (!_jumpScarePlayed && distanceToPlayer <= catchingRange)
         {
+            firstPersonController.enabled = false;
+            rb.isKinematic = false;
+            Entity.enabled = false;
+            LookAtAndRaiseCamera();
             StartCoroutine(PlayJumpScareAndReload());
         }
-        else if (_jumpScarePlayed && destination > catсhingRange)
+        else if (_jumpScarePlayed && distanceToPlayer > catchingRange)
         {
-            // Позволяет монстру снова испугать игрока, если тот ушёл на безопасное расстояние
             _jumpScarePlayed = false;
         }
 
-        if (destination <= chaseRange)
+        if (distanceToPlayer <= chaseRange)
         {
             Entity.destination = player.position;
         }
@@ -45,11 +58,20 @@ public class ShadowAI : MonoBehaviour
         }
     }
 
+    void LookAtAndRaiseCamera()
+    {
+        Vector3 cameraPosition = mainCamera.position;
+        mainCamera.LookAt(Entity.transform.position);
+        cameraPosition.y += heightOffset;
+        mainCamera.position = cameraPosition;
+    }
+
     private IEnumerator PlayJumpScareAndReload()
     {
-        _jumpScarePlayed = true; // Устанавливаем флаг, что звук проигрывается
+        _jumpScarePlayed = true;
         jumpscare.Play();
-        yield return new WaitForSeconds(jumpscare.clip.length); // Ожидаем окончания проигрывания звука
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Перезагружаем текущую сцену
+
+        yield return new WaitForSeconds(jumpscare.clip.length);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
