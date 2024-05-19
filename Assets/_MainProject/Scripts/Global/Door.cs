4e7790c65b20d8ca29b58957264d941957a7364c
Kiseluie <timurkirdakov@gmail.com>
2024-05-19 19:35:52 +0300

    Doors added

---
+++ Assets/_MainProject/Scripts/Global/Door.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private GameObject player;
+    private Component[] meshes;
+
+    public bool isOpening;
+
+    private void Start()
+    {
+        player = GameObject.Find("FirstPersonController");
+        meshes = gameObject.GetComponentsInChildren<MeshCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) <= 3) 
+        {
+            if (Input.GetKeyDown(KeyCode.E) && isOpening == false)
+            {
+                StartCoroutine(OpenDoor());
+            }
+        }
+    }
+
+    IEnumerator OpenDoor() 
+    {
+        isOpening = true;
+        gameObject.GetComponent<Animator>().Play("Door");
+        yield return new WaitForSeconds(0.05f);
+        foreach (MeshCollider mesh in meshes)
+        {
+            mesh.enabled = false;
+        }
+        yield return new WaitForSeconds(1f);
+        foreach (MeshCollider mesh in meshes)
+        {
+            mesh.enabled = true;
+        }
+        yield return new WaitForSeconds(4.0f);
+
+        gameObject.GetComponent<Animator>().Play("NewState");
+        isOpening = false;
+    }
+}
