d3b6a147b06920d5059293272d894384a6728b3c
ArtyMel <artymeleteev@gmail.com>
2024-05-21 17:46:22 +0300

    Дверь в спортзал открывается ключом

--- Assets/_MainProject/Scripts/Global/RayScript.cs
+++ Assets/_MainProject/Scripts/Global/RayScript.cs
@@ -8,6 +8,7 @@ public class RayScript : MonoBehaviour
     public GameObject CollectText;
     public GameObject player;
     private Camera camera;
+    public GameObject Door;
     private void Start()
     {
         CollectText.SetActive(false);
@@ -19,9 +20,26 @@ public class RayScript : MonoBehaviour
         Ray ray = camera.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit, distance))
         {
-            if (hit.collider.tag == ("Battery"))//какой тег
+            if (hit.collider.tag == ("Battery") || hit.collider.tag == ("Key"))//появляется надпись "press E to collect"
             {
                 CollectText.SetActive(true);
+            }
+            else
+            {
+                CollectText.SetActive(false);
+            }
+            if(hit.collider.tag == ("Key"))//подбор ключа
+            {
+                var _lock = Door.GetComponent<LockDoor>();
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    Destroy(hit.collider.gameObject);
+                    CollectText.SetActive(false);
+                    _lock.Key++;
+                }
+            }
+            if (hit.collider.tag == ("Battery"))//подбор батарейки
+            {
                 var _addBattery = player.gameObject.GetComponent<Flashlight>();
                 if (Input.GetKeyDown(KeyCode.E) && _addBattery.batteryCount < _addBattery.maxBatteryCount)
                 {
@@ -29,10 +47,6 @@ public class RayScript : MonoBehaviour
                     Destroy(hit.collider.gameObject);
                     CollectText.SetActive(false);
                 }
-            }
-            else
-            {
-                CollectText.SetActive(false);
             }
         }
     }
